3b7bc8e6a563578e2f0de5a84fc7da96132b31a8
aspfit <32666532+aspfit@users.noreply.github.com>
2017-10-26 11:22:09 +0200

    Create runnable.cpp

---
+++ runnable.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+struct Queue {
+	int queue[10];
+	int front = -1;
+	int rear = -1;
+
+	void Enqueue(int info) {
+		if (isEmpty())
+			front = rear = 0;
+		else if (isFull()) { cout << "INFO :: Queue is FULL!" << endl; return; }
+		else
+			rear++;
+		queue[rear] = info;
+	}
+	int Dequeue() {
+		auto temp = -1;
+		if (isEmpty())
+			throw new exception("Queue is empty!");
+		if (front == rear) {
+			temp = queue[front];
+			front = rear = -1;
+			return temp;
+		}
+		else
+			temp = queue[front];
+		front++;
+		return temp;
+	}
+	void PrintQueue() {
+		if (!isEmpty())
+			for (int i = front; i <= rear; i++)
+				cout << queue[i] << endl;
+
+	}
+	void PrintAll() {
+		for (int i = 0; i <= 10; i++)
+			cout << queue[i] << endl;
+	}
+	void PrintIndexStatus() {
+		cout << "-----------\nFront: " << front << "\nRear: " << rear << "\n-----------\n";
+	}
+	bool isEmpty() {
+		return front == -1;
+	}
+	bool isFull() {
+		return rear == (sizeof(queue) / sizeof(int)) - 1;
+	}
+};
+
+int main() {
+	auto queue = new Queue;
+	queue->Enqueue(4);
+	queue->Enqueue(3);
+	queue->Enqueue(5);
+	queue->Dequeue();
+	queue->Dequeue();
+	queue->Dequeue();
+	cout << "IsEmpty " << boolalpha << queue->isEmpty() << endl;
+	cout << "IsFull " << boolalpha << queue->isFull() << endl;
+	queue->PrintIndexStatus();
+	queue->PrintQueue();
+	queue->PrintAll();
+
+
+	getchar();
+	return 0;
+}
